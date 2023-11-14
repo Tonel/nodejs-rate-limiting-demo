8b32f23af9afed6a0454b7ff934775ffecf57e28
Antonello Zanini <antonello@writech.run>
2023-11-14 09:28:43 +0100

    Rate limiting logici implemented

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const { rateLimit } = require("express-rate-limit");
 
 const port = 3000;
 
 // initialize an Express server
 const app = express();
+
+// define the rate limiting middleware
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    limit: 10, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
+    standardHeaders: true, // add the `RateLimit-*` headers to the response
+    legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
+});
+
+// apply the rate limiting middleware to all endpoints
+app.use(limiter);
 
 // define a sample endpoint
 app.get("/hello-world", (req, res) => {
