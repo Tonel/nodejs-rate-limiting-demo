32550cd4a3b7a92821264bcf85c3287a67d98359
Antonello Zanini <antonello@writech.run>
2023-11-14 09:37:16 +0100

    Slowing down rate limiting logic implemented

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const { slowDown } = require("express-slow-down");
 
 const port = 3000;
 
 // initialize an Express server
 const app = express();
+
+// define the behavior of the slowing down rate limiting middleware
+const limiter = slowDown({
+    windowMs: 15 * 60 * 1000, // 5 minutes
+    delayAfter: 10, // Allow 10 requests per `windowMs` (5 minutes) without slowing them down
+    delayMs: (hits) => hits * 200, // add 200 ms of delay to every request after the 10th
+    maxDelayMs: 5000 // max delay of 5 seconds
+});
+
+// register the slowing down rate limiting middleware to all endpoints
+app.use(limiter);
 
 // define a sample endpoint
 app.get("/hello-world", (req, res) => {
