83cdfeb4481c370a35779b9802053b3fbdd0924c
Antonello Zanini <antonello@writech.run>
2023-11-14 14:03:25 +0100

    Updated some comments

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express();
 // define the behavior of the slowing down rate limiting middleware
 const limiter = slowDown({
     windowMs: 15 * 60 * 1000, // 5 minutes
-    delayAfter: 10, // Allow 10 requests per `windowMs` (5 minutes) without slowing them down
+    delayAfter: 10, // allow 10 requests per `windowMs` (5 minutes) without slowing them down
     delayMs: (hits) => hits * 200, // add 200 ms of delay to every request after the 10th
-    maxDelayMs: 5000 // max delay of 5 seconds
+    maxDelayMs: 5000 // max global delay of 5 seconds
 });
 
 // register the slowing down rate limiting middleware to all endpoints
